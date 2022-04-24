5a5ab5a400ed686c07e521dfe5031f7b69b50bf4
Heather <heather@carraig.ai>
2022-04-24 21:15:56 +0100

    function names to camelCase per style convention

--- countdown.hs
+++ countdown.hs
@@ -1,8 +1,6 @@
 -- Channel 4 countdown solver
 -- Given a set of numbers, and solution, figure out how the numbers were combined to arrive at solution
 
--- TODO:
--- fix names (from python like)
 -- show (Plus (Raw 15) (Multiply ( Minus (Raw 5) (Raw 1) ) (Raw 2)))
 -- "(15+((5-1)*2))"  
 data Exp = Raw Int | Plus Exp Exp | Minus Exp Exp | Multiply Exp Exp | Divide Exp Exp deriving Eq
@@ -23,25 +21,25 @@ splits xs = splits' (head xs) (tail xs)
         splits' n xs  = (n,xs) : splits' (head xs) (tail xs)
 
 
--- convert_to_expression (25,[6,9,1,8,3])
-convert_to_expression :: (Int, [Int]) -> (Exp, [Exp])
-convert_to_expression (x, ys) = ( Raw x, map (\y -> Raw y) ys )
+-- convertToExpression (25,[6,9,1,8,3])
+convertToExpression :: (Int, [Int]) -> (Exp, [Exp])
+convertToExpression (x, ys) = ( Raw x, map (\y -> Raw y) ys )
 
 -- this function will evaluate a/b and b/a before creating expression.
-division_helper :: Exp -> Exp -> [Exp] -> [(Exp, [Exp])]
-division_helper a b xs
-    | eb > ea      = division_helper b a xs
+divisionHelper :: Exp -> Exp -> [Exp] -> [(Exp, [Exp])]
+divisionHelper a b xs
+    | eb > ea      = divisionHelper b a xs
     | otherwise    = if ea `mod` eb == 0 then [(Divide a b, [])] else []
-    where ea = eval_expression a
-          eb = eval_expression b
+    where ea = evalExpression a
+          eb = evalExpression b
 
--- create_expressions (25,[6,9,1,8,3])
-create_expressions :: (Exp, [Exp]) -> [(Exp, [Exp])]
-create_expressions (x, [])  = [(x, [])]
-create_expressions (x, ys)  = 
+-- createExpressions (25,[6,9,1,8,3])
+createExpressions :: (Exp, [Exp]) -> [(Exp, [Exp])]
+createExpressions (x, [])  = [(x, [])]
+createExpressions (x, ys)  = 
         let 
          step = pair' x ys 0
-        in concatMap create_expressions step ++ step
+        in concatMap createExpressions step ++ step
 
     where pair' x ys n  
             | n == length ys       = []
@@ -51,28 +49,27 @@ create_expressions (x, ys)  =
                    plus_exp'          = ( (Plus  x (ys!!n)), rest')
                    minus_exp'         = ( (Minus x (ys!!n)), rest')
                    multiply_exp'      = ( (Multiply x (ys!!n)), rest')
-                   division_exp'      = division_helper x (ys!!n) rest' 
+                   division_exp'      = divisionHelper x (ys!!n) rest' 
                  in 
                    division_exp' ++ [ plus_exp',  minus_exp', multiply_exp' ] ++ pair' x ys (n+1)
 
 
--- eval_expression
+-- evalExpression
 -- reduce a `mod` reduce b == 0
-
-eval_expression :: Exp -> Int
-eval_expression ( Raw a )        =  a
-eval_expression ( Plus a b)      = eval_expression a + eval_expression b
-eval_expression ( Minus a b)     = eval_expression a - eval_expression b
-eval_expression ( Multiply a b)  = eval_expression a * eval_expression b
-eval_expression ( Divide a b)    = eval_expression a `div` eval_expression b
-
+evalExpression :: Exp -> Int
+evalExpression ( Raw a )        =  a
+evalExpression ( Plus a b)      = evalExpression a + evalExpression b
+evalExpression ( Minus a b)     = evalExpression a - evalExpression b
+evalExpression ( Multiply a b)  = evalExpression a * evalExpression b
+evalExpression ( Divide a b)    = evalExpression a `div` evalExpression b
 
 
 
--- countdown_solver [25,6,9,1,8,3] 52
--- countdown_solver [25,6,9,1,8,3] 303
--- countdown_solver [25,6,9,1,8,3] 26
--- countdown_solver [100, 4, 5, 10] 20
-countdown_solver :: [Int] -> Int -> Exp
-countdown_solver nums target = fst $ head $ filter (\(x,y) ->  eval_expression x == target) . concatMap create_expressions . map convert_to_expression $ splits nums
+
+-- countdownSolver [25,6,9,1,8,3] 52
+-- countdownSolver [25,6,9,1,8,3] 303
+-- countdownSolver [25,6,9,1,8,3] 26
+-- countdownSolver [100, 4, 5, 10] 20
+countdownSolver :: [Int] -> Int -> Exp
+countdownSolver nums target = fst $ head $ filter (\(x,y) ->  evalExpression x == target) . concatMap createExpressions . map convertToExpression $ splits nums
 
